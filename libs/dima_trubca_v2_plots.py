c6a9753e4cc00f992d8f0dddeee0e8e55b07c959
Dumitru Cretu <37322085+filpatterson@users.noreply.github.com>
2021-11-15 17:11:47 +0200

    updating uniswap v2 pools data analysis

--- libs/dima_trubca_v2_plots.py
+++ libs/dima_trubca_v2_plots.py
@@ -39,32 +39,51 @@ def show_swaps_count_moving_averages(swaps_df: pd.DataFrame, pool_name: str, x_s
     fig.legend()
     
 
-def show_swaps_reserves_evolution_through_time(swaps_df: pd.DataFrame, first_token_symbol: str, second_token_symbol: str, 
-                                               pool_name: str, x_size: int=10, y_size=5):
-    fig, ax1 = plt.subplots(figsize=(x_size, y_size))
+# def show_swaps_reserves_evolution_through_time(swaps_df: pd.DataFrame, first_token_symbol: str, second_token_symbol: str, 
+#                                                pool_name: str, first_label_name: str, second_label_name=str, 
+#                                                x_size: int=10, y_size: int=5):
+#     fig, ax1 = plt.subplots(figsize=(x_size, y_size))
 
-    ax2 = ax1.twinx()
+#     ax2 = ax1.twinx()
+#     fig.autofmt_xdate()
+
+#     ax1.plot_date(swaps_df.date, swaps_df.reserve0, linestyle='solid', marker=None, label=first_token_symbol)
+#     ax2.plot_date(swaps_df.date, swaps_df.reserve1, linestyle='solid', marker=None, color='red', label=second_token_symbol)
+
+#     ax1.set_title("Daily reserve values for pool " + pool_name)
+#     fig.legend()
+
+#     ax1.set_ylabel(first_label_name)
+#     ax2.set_ylabel(second_label_name)
+
+def show_swaps_reserves_evolution_through_time(swaps_df: pd.DataFrame, first_token_reserve_name: str, second_token_reserve_name: str, 
+                                               x: int=10, y: int=5):
+    fig = plt.figure(figsize=(x, y))
+    
+    plt.subplot(1, 2, 1)
+    sns.lineplot(data=swaps_df, x='date', y='reserve0', color='green').set_title(first_token_reserve_name)
+    plt.subplot(1, 2, 2)
+    sns.lineplot(data=swaps_df, x='date', y='reserve1', color='red').set_title(second_token_reserve_name)
+    
     fig.autofmt_xdate()
-
-    ax1.plot_date(swaps_df.date, swaps_df.reserve0, linestyle='solid', marker=None, label=first_token_symbol)
-    ax2.plot_date(swaps_df.date, swaps_df.reserve1, linestyle='solid', marker=None, color='red', label=second_token_symbol)
-
-    ax1.set_title("Daily reserve values for pool " + pool_name)
-    fig.legend()
-
-    ax1.set_ylabel(first_token_symbol)
-    ax2.set_ylabel(second_token_symbol)
+    plt.show()
     
     
-def show_pool_price_evolution_from_reserves(df: pd.DataFrame, first_arg: str, second_arg: str, 
-                                            pool_name: str, x_size: int=10, y_size: int=5):
-    df['price'] = df[first_arg] / df[second_arg]
-    fig, ax1 = plt.subplots(figsize=(15, 9))
+def show_pool_price_evolution_from_reserves(df: pd.DataFrame, first_token_price_name: str, second_token_price_name: str, 
+                                            x: int=10, y: int=5, hspace: float=0.25, wspace: float=0.25):
+    fig = plt.figure(figsize=(x, y))    
+    
+    df['first_price'] = df['reserve0'] / df['reserve1']
+    df['second_price'] = df['reserve1'] / df['reserve0']
+    
+    plt.subplot(1, 2, 1)
+    sns.lineplot(data=df, x='date', y='first_price', color='green').set_title(first_token_price_name)
+    plt.subplot(1, 2, 2)
+    sns.lineplot(data=df, x='date', y='second_price', color='red').set_title(second_token_price_name)
 
-    ax2 = ax1.twinx()
-
-    ax1.plot_date(df.date, df.price, linestyle='solid', marker=None, label='Number of transactions')
-    ax1.set_title("Price " + pool_name)
+    fig.autofmt_xdate()
+    plt.subplots_adjust(wspace=wspace, hspace=hspace)
+    plt.show()
     
     
 def show_swaps_amount_in_moving_averages(swaps_df: pd.DataFrame, pool_name: str, x_size: int=10, y_size: int=5):
